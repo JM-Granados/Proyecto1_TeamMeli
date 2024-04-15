3165efc895abc59d9385f147412e5579015bdcd4
Jose <josemacr04@gmail.com>
2024-04-14 21:49:20 -0600

    Update CreateDataSet.jsx

--- frontend/client/src/Pages/CreateDataSet.jsx
+++ frontend/client/src/Pages/CreateDataSet.jsx
@@ -17,42 +17,38 @@ function CreateDataSet() {
                     </ul>
                 </div>
                 <div className="infoinput">
-                  <ul>
-                    <li><input
-                        type="text"
-                        placeholder="Name of your dataset"
-                        autoComplete="off"
-                        name="datasetName"
-                        className="form-control rounded-0"
-                        
-                    /></li>
-                     <li><input
-                        type="text"
-                        placeholder="Description of your dataset"
-                        autoComplete="off"
-                        name="datasetDescription"
-                        className="form-control rounded-0"
-                       
-                    /></li>
-                    <li><label htmlFor="avatarInput" className="folderButton">
-                        <img src="assets/archive.svg" alt="Select archive" />
-                        photo
-                        <input
-                            type="file"
-                            id="avatarInput"
-                            accept="image/png, image/jpeg, image/jpg"
-                          
-                        />
-                    </label></li>
-                   </ul>
+                    <ul>
+                        <li><input
+                            type="text"
+                            placeholder="Name of your dataset"
+                            autoComplete="off"
+                            name="datasetName"
+                            className="form-control rounded-0"
+                        /></li>
+                        <li><input
+                            type="text"
+                            placeholder="Description of your dataset"
+                            autoComplete="off"
+                            name="datasetDescription"
+                            className="form-control rounded-0"
+                        /></li>
+                        <li><label htmlFor="avatarInput" className="folderButton">
+                            <img src="assets/archive.svg" alt="Select archive" />
+                            photo
+                            <input
+                                type="file"
+                                id="avatarInput"
+                                accept="image/png, image/jpeg, image/jpg"
+                            />
+                        </label></li>
+                    </ul>
                     <li><label htmlFor="avatarInput" className="folderButton">
                         <img src="assets/archive.svg" alt="Select archive" /> archive
-                       
+                    
                         <input
                             type="file"
                             id="archiveInput"
                             accept="*"
-                          
                         />
                     </label></li>
                     <li><label htmlFor="avatarInput" className="folderButton">
@@ -62,14 +58,13 @@ function CreateDataSet() {
                             type="file"
                             id="avatarInput"
                             accept="video/*"
-                          
                         />
                     </label></li>
                 </div>
                 
                 <div className="createButton">
                     <button /*onClick={handleCreate}*/>Create Data Set</button>
-                </div>*
+                </div>
 
             </div>
         
