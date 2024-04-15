3b59c7c5487adfc6c8eed3b12070b5b474720f1d
Jose <josemacr04@gmail.com>
2024-04-15 03:45:10 -0600

    buenas nochessss

---
+++ frontend/client/src/Mensajes/Dashboard/dashboard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const dashboard = () => {
+    return (
+        <div class="container-fluid h-100">
+            <div class="row h-100">
+                <div class="col-4 border">
+        
+                </div>
+                <div class="col-4 border">
+
+                </div>
+                <div class="col-4 border">
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default dashboard;
--- frontend/client/src/Pages/Dm.jsx
+++ frontend/client/src/Pages/Dm.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
+import Dash from '../Mensajes/Dashboard/dashboard';
 
 function Dm() {
     return (
         <div>
             <NavBar/>
+            <Dash/>
         </div>
     )
 }
